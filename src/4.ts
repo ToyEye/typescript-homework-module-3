7648c247a896ed81d304b28182939d855e8aedbc
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2023-09-05 16:43:12 +0300

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,65 @@
+type TDoor = "open" | "close";
+
+interface IHouse {
+  door: "open" | "close";
+  //   key: object;
+}
+
+abstract class House implements IHouse {
+  public door: TDoor;
+
+  protected tenants: Person[] = [];
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Людина ввійшла в будинок.");
+    } else {
+      console.log("Двері закриті. Людина не може ввійти.");
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+class MyHouse extends House {
+  public key;
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      console.log("Двері відчинені.");
+    } else {
+      console.log("Ключ не підходить. Двері залишаються закритими.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +68,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
